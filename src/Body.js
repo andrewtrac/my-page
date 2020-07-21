36136ca206109b246e5866a2afd5813b4059c513
Andrew Trac <andrewtrac@Andrews-Air.Home>
2020-07-21 12:25:59 -0400

    setup page with navbar and body

---
+++ src/Body.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+export default function Body() {
+
+
+return (
+  <>
+  <p>Hello World</p>
+  </>
+
+)
+
+
+}
---
+++ src/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './styles/Navbar.scss'
+
+export default function Navbar() {
+
+
+
+
+  return (
+
+    <div className="Navbar">
+    <p>Nav Bar</p>
+    </div>
+
+
+  )
+
+
+
+}

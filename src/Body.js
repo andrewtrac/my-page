1dadc9c89fd277cadc0e82011815de06f26531f5
Andrew Trac <andrewtrac@Andrews-Air.Home>
2020-07-21 18:35:39 -0400

    refined the portfolio page

--- src/Body.js
+++ src/Body.js
@@ -1,17 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
 import "./styles/Header.scss";
 import "./styles/Body.scss";
 import "./styles/Portfolio.scss"
 
 export default function Body() {
+
+const [portfolio, setPortfolio] = useState("")
+
+console.log(portfolio)
+
   return (
     <div className="Body">
-    <div className="Header">
+
+    {portfolio === "tweeter" && <div className="Header__tweeter" onClick={e => setPortfolio("")}><span>Tweeter</span></div>}
+    {portfolio === "" &&  <div className="Header">
       <span style={{fontSize: "60px"}}>Portfolio</span>
-    </div>
+    </div>}
     <div className="Portfolio">
-      <div className="Portfolio__tweeter"></div>
-      <div className="Portfolio__tweeter"></div>
+      <div className="Portfolio__tweeter" onClick={e => setPortfolio("tweeter")}></div>
+      <div className="Portfolio__tweeter" onClick={e => setPortfolio("mapbook")}></div>
       <div className="Portfolio__tweeter"></div>
       <div className="Portfolio__tweeter"></div>
     </div>
